53905dba4fbc15813da12cff2d1d097cf74604e7
Arpit kumar <163999838+arpit-kum@users.noreply.github.com>
2024-03-20 10:26:58 +0000

    data

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"Use strict";// treat all js as newer version
+
+//alert(3 + 3)//we are using nodejs,not browser
+
+// console.log(3 + 3)
+// console.log("hitesh")
+// let name ="arpit"
+// let age = 20
+// let isLoggefId = false
+
+//type of datatype in javascript
+
+//it is primitive datatype
+//1. number 
+//2.bigint
+//3.string =>""
+//4.boolean=>true/false
+//5.null=> this is standalone value
+//6.undefined =>
+//7.symbol=>unique
+
+
+//no-primitive datatype
+//1.object
+console.log(typeof arpit);
+
